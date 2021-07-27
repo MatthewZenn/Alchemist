b709d13c227f8fcd44079eba249c0646dbd223e4
Matthew Zenn <spyguy57@gmail.com>
2021-07-27 17:38:53 -0400

    initialized app

---
+++ logic.js
@@ -0,0 +1,24 @@
+var maximize = document.getElementById("maximize")
+
+maximize.addEventListener("click", Maximize)
+
+function Maximize() {
+    if (window.innerWidth == screen.width) {
+        window.resizeTo(1280, 720);
+        document.getElementById("circle").innerHTML = "&triangle;"
+    }
+    else {
+        window.moveTo(0, 0);
+        window.resizeTo(screen.width, screen.height);
+        document.getElementById("circle").innerHTML = "&triangledown;"
+    }
+}
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth == screen.width) {
+        document.getElementById("circle").innerHTML = "&triangledown;"
+    }
+    else {
+        document.getElementById("circle").innerHTML = "&triangle;"
+    }
+});
